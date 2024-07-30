5df7720e6ed82d89d53bb952c0c6464cb8c944bd
quockhanh0212 <quockhanh.021220@gmail.com>
2024-07-31 01:45:31 +0700

    add implementation

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template, flash, redirect, request, session, url_for
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from config import Config
 from FlaskWebProject import app, db
 from FlaskWebProject.forms import LoginForm, PostForm
@@ -67,10 +67,12 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.error('Authentication error')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info('Successful login: ' + user.username)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
@@ -85,11 +87,14 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = None
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+            request.args['code'],
+            scopes=Config.SCOPE,
+            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
+        app.logger.info('Successful login')
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
         user = User.query.filter_by(username="admin").first()
@@ -111,18 +116,22 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
-    pass
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    return msal.ConfidentialClientApplication(
+        Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
+        client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
--- WRITEUP.md
+++ WRITEUP.md
@@ -7,6 +7,25 @@
 - *Choose the appropriate solution (VM or App Service) for deploying the app*
 - *Justify your choice*
 
+Virtual Machine:
+- Costs: fixed monthly operating costs according to the VM size, but including also hidden costs for maintenance of the Operating System
+- Scalability: Can scale both vertically and horizontally through the use of a load balancer
+- Availability: If multiple are loadbalanced, this is not an issue, otherwise downtime for patching should be taken into account.
+- Workflow: Provision virtual machine, log into machine to pull in application files and launch the application. Alternatively a configuration management tool like Chef or Puppet could help to automatically deploy the application to the VM, avoiding the need for manual config.
+
+App Service:
+- Costs: Lower costs, can leverage shared infrastructure. No operational costs for OS patching
+- Scalability: A bit less than VMs, depending on the App Service Plan SKU (e.g. manual scale to 3 on B1 SKU). Vertical scaling limit lower than a VM.
+- Availability: High available, no downtime for patching
+- Workflow: CD Integration with Github, Bitbucket, local git for upload of the code. Can leverage staging slots for testing deployments.
+
+In this scenario I would use the App Service.
+The application is fairly lightweight, so vertical scaling will not be required. Using an App Service Plan with sufficient horizontal scaling for the envisioned load (which is very low in our case).
+
 ### Assess app changes that would change your decision.
 
-*Detail how the app and any other needs would have to change for you to change your decision in the last section.* 
+If the application would include processing that requires heavy compute resources, such as e.g. PDF rendering from various CMS articles on the server-side. Rather than using a VM for this, we could look into Azure Functions or Azure Batch to spin up on-demand.
+
+An alternative reason could be if the data confidentiality/regulations require dedicated hardware. An App Service Environment (isolated) might be a solution here.
+
+Or a need to run a specific operating system or runtime stack not supported by App Service.
